47146a502202c3ed319b6738b347b604fdeb9485
Vedant Singh <vedantsingh4002@gmail.com>
2026-04-01 15:32:35 +0530

    files added

---
+++ March29/WebAPI/WebAPI/Controllers/EmpController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.netcoreMvcDemo;
+using WebApi.netcoreMvcDemo.Models;
+using WebApi.netcoreMvcDemo.Models;
+using WebAPI.Models;
+
+namespace WebApi.netcoreMvcDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpController : ControllerBase
+    {
+        private readonly IEmployee _employeeService;
+        public EmpController(IEmployee employeeService)
+        {
+            _employeeService = employeeService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> GetAll(int page = 1, int pageSize = 5)
+        {
+
+            return Ok(await _employeeService.GetAllEmployeesAsync(page, pageSize));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetById(int id)
+        {
+            var employee = await _employeeService.GetEmployeeByIdAsync(id);
+            if (employee == null)
+                return NotFound("Employee not found");
+            return Ok(employee);
+        }
+        [HttpGet("basic")]
+        public async Task<ActionResult<List<EmployeeBasicDto>>> GetBasicEmployeeList(int page = 1, int pageSize = 5)
+        {
+            var result = await _employeeService.GetAllEmployeeBasicInfoAsync(page, pageSize);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> Create([FromForm] Employee employee,
+            IFormFile? image) //added ? here means making it nullable 
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var added = await _employeeService.AddEmployeeAsync(employee, image);
+            return Ok(added);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Employee>> Update(
+        int id, [FromForm] EmployeeUpdateDto employeeDto, IFormFile? image)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            // map dto to entity
+            var employee = new Employee
+            {
+                Id = id, // take from route only
+                FirstName = employeeDto.FirstName,
+                LastName = employeeDto.LastName,
+                Email = employeeDto.Email,
+                Age = employeeDto.Age,
+                ImagePath = employeeDto.ImagePath
+            };
+
+            var updated = await _employeeService.UpdateEmployeeAsync(employee, image);
+            if (updated == null)
+                return NotFound("Employee not found to update");
+
+            return Ok(updated);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Employee>> Delete(int id)
+        {
+            var deleted = await _employeeService.DeleteEmployeeAsync(id);
+            if (deleted == null)
+                return NotFound("Employee not foudn to delete");
+            return Ok(deleted);
+        }
+
+
+    }
+}
---
+++ March29/WebAPI/WebAPI/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.netcoreMvcDemo.Models;
+
+namespace WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<EmpContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("constring")));
+            builder.Services.AddScoped<IEmployee, EmployeeService>();
+            builder.Services.AddControllers();
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
+            var app = builder.Build();
+
+            app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+            app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.Run();
+        }
+    }
+}
