65a388dfd07497ed2143852976992c4d46355cb3
Vedant Singh <vedantsingh4002@gmail.com>
2026-04-02 10:43:54 +0530

    files added

---
+++ March27/WebAPI/WebAPI/IEmployee.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using WebAPI.Models;
+namespace WebAPI
+{
+    public interface IEmployee
+    {
+        Task<Employee> AddEmployeeAsync(Employee employee, IFormFile? image);
+        Task<Employee?> UpdateEmployeeAsync(Employee employee, IFormFile? image);
+        Task<Employee?> DeleteEmployeeAsync(int id);
+        Task<Employee?> GetEmployeeByIdAsync(int id);
+        Task<List<Employee>> GetAllEmployeesAsync(int pageNumber, int pageSize);
+
+        Task<List<EmployeeBasicDto>> GetAllEmployeeBasicInfoAsync(int pageNumber, int pageSize, string? searchTerm);
+    }
+}
---
+++ March27/WebAPI/WebAPI/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<EmpContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("constring")));
+            builder.Services.AddScoped<IEmployee, EmployeeService>();
+            builder.Services.AddControllers();
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
+            var app = builder.Build();
+
+            app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.UseStaticFiles();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+            app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.Run();
+        }
+    }
+}
