d2c068e26c97d3521a7df109bb6a8c26cb14f8bc
Vedant Singh <vedantsingh4002@gmail.com>
2026-04-03 14:31:46 +0530

    file added

---
+++ March31/WebAPI/WebAPI/Models/Empcontext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.netcoreMvcDemo.Models;
+
+namespace WebApi.netcoreMvcDemo.Models
+{
+    public class EmpContext : DbContext
+    {
+        public EmpContext(DbContextOptions dbContextOptions) :
+            base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Employee> employees { set; get; }
+
+    }
+}
